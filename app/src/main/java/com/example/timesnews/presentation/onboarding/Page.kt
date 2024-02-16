ae54ea5f1fb1aa197a6465193637f8243e1abc9f
SajalJana2004 <kakalijana1254@gmail.com>
2024-02-16 05:51:29 +0530

    Updates

--- app/src/main/java/com/example/timesnews/presentation/onboarding/Page.kt
+++ app/src/main/java/com/example/timesnews/presentation/onboarding/Page.kt
@@ -5,24 +5,20 @@ import com.example.timesnews.R
 
 data class Page(
     val title: String,
-    val description: String,
     @DrawableRes val image: Int
 )
 
 val pages = listOf(
     Page(
         title = "Tech Titans Clash: Apple and Google's Antitrust Battle",
-        description = "",
         image = R.drawable.onboarding1,
     ),
     Page(
         title = "Climate Crisis Intensifies: UN Report Warns of Dire Consequences",
-        description = "",
         image = R.drawable.onboarding2,
     ),
     Page(
         title = "Space Race Heats Up: SpaceX's Ambitious Plans for Mars Colonization",
-        description = "",
         image = R.drawable.onboarding3,
     )
 )
